cafd74fbd9bb614b09ce6f5a0e248af98d297423
alu0101559513 <alu0101559513@ull.edu.es>
2024-04-12 12:19:06 +0100

    mejora

---
+++ proyecto/src/info_biblio.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+void get_informacion() {
+  std::cout << "📖: Biblioteca";
+  std::cout << "📍: San Cristóbal de La Laguna";
+  std::cout << "📞: 922 421 686";
+  std::cout << "🗓️: Lunes-Viernes (9:00 - 14:00)";
+}
