d7507114e12326d90e7fd668f6e640fadce75ab4
Laura <alu0101568935@ull.edu.es>
2024-04-18 17:55:08 +0100

    actualización

--- proyecto/library/menu.h
+++ proyecto/library/menu.h
@@ -1,5 +1,11 @@
 #include <iostream>
 #include "../library/usuario.h"
+#include "horario.h"
+#include "aulas.h"
+#include "libros.h"
+#include "info_biblio.h"
+#include "discusion.h"
+#include "foro.h"
 
 bool MostrarMenu(bool);
-bool MenuRegistrado(bool, Usuario& usuario);
+void MenuRegistrado(Usuario&, Horario);
