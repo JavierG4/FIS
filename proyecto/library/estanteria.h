b4092e736506dc3d299e46726a0a5a15aaa22c34
gabmartinbr <alu0101539157@ull.edu.es>
2024-04-23 11:03:13 +0100

    actualización

---
+++ proyecto/library/estanteria.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "libros.h"
+
+class Estanteria {
+public:
+  Estanteria();
+  ~Estanteria();
+
+  void cargar_catalogo();
+  void mostrar_catalogo();
+
+private:
+  std::vector<Libro> catalogo_;
+  std::string ruta_archivo_libro_;
+};
